00fb8fbb43bf2e9493fb79e6718aef7e3ae2f5ff
Dang Tran Khanh <khanhtran28092000@gmail.com>
2021-05-03 09:00:06 +0700

    update file

--- lib/Sensor/view/sensors_screen.dart
+++ lib/Sensor/view/sensors_screen.dart
@@ -3,7 +3,6 @@ import 'package:study_space/Home/view/side_menu.dart';
 import 'package:study_space/Sensor/view/body.dart';
 import 'package:study_space/constants.dart';
 
-
 class SensorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
