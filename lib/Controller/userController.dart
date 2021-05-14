ad1f3a7ff8d1c6943f949aa28d50f0289e9d01af
nguyenbao2301 <66950291+nguyenbao2301@users.noreply.github.com>
2021-05-14 23:06:40 +0700

    test user controller

--- lib/Controller/userController.dart
+++ lib/Controller/userController.dart
@@ -1,26 +1,30 @@
 import 'package:mysql1/mysql1.dart';
+import 'dart:async';
+
 import 'package:study_space/constants.dart';
-class UserController {
-  Future<MySqlConnection> connect() async{ //connect to db
-    var settings = new ConnectionSettings(
-        host: db_host,
-        port: db_port,
-        user: db_user,
-        password: db_password,
-        db: db_name
-    );
-    var conn = await MySqlConnection.connect(settings);
-    return conn;
-  }
-  Future<void> disconnect(MySqlConnection conn) async{ //disconnect from db
-      await conn.close();
-  }
-  Future<void> testSql() async {
-    var conn = await this.connect();
-    var results = await conn.query('SELECT * FROM users');
+import 'package:study_space/Model/user.dart';
+class userController {
+  var conn;
+  var settings = new ConnectionSettings(
+      host: db_host,
+      port: db_port,
+      user: db_user,
+      password: db_password,
+      db: db_name
+  );
+  userController() {print('constructed');}
+
+  Future<String> testSql() async {
+    String s = '';
+    print('in test');
+    var conn  = await MySqlConnection.connect(settings);
+    print('con done');
+    var results =await conn.query('SELECT * FROM users;');
     for (var row in results) {
-      print('id: ${row[0]}, username: ${row[1]} ');
+      s = s + 'id: ${row[0]}, username: ${row[1]} ';
+      print(s);
     }
-    this.disconnect(conn);
+    await conn.close();
+    return s;
   }
 }
