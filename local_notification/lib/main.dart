2d9ba8f9c1dc364f62fc43dc2ebd30926d29f18e
dirtyg2120 <57407829+dirtyg2120@users.noreply.github.com>
2021-05-17 21:33:08 +0700

    Multiple scheduled notifications

--- local_notification/lib/main.dart
+++ local_notification/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:local_notification/time_picker.dart';
 
 void main() {
   runApp(
@@ -16,12 +15,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-  DateTime scheduledTime = DateTime.parse("2021-05-14 15:06:00");
   @override
   initState() {
     super.initState();
-    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    // If you have skipped STEP 3 then change app_icon to @mipmap/ic_launcher
     var initializationSettingsAndroid =
         new AndroidInitializationSettings('app_icon');
     var initializationSettings =
@@ -53,22 +49,15 @@ class _MyAppState extends State<MyApp> {
       platformChannelSpecifics,
       payload: 'Ra chơi 15 phút',
     );
-    // await flutterLocalNotificationsPlugin.zonedSchedule(
-    //   2,
-    //   'Break rồiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-    //   'Giờ chời đến rồi giờ chơi đến rồi, đi chơi thôi',
-    //   tz.TZDateTime(tz.local, dateTime.year, dateTime.month, dateTime.hour,
-    //       dateTime.minute),
-    //   platformChannelSpecifics,
-    //   androidAllowWhileIdle: true,
-    //   uiLocalNotificationDateInterpretation:
-    //       UILocalNotificationDateInterpretation.absoluteTime,
-    //   payload: 'Ra chơi 15 phút',
-    // );
   }
 
+  List<String> scheduleList = [
+    "2021-05-17 21:29:00",
+    "2021-05-17 21:29:30",
+    "2021-05-17 21:30:00"
+  ];
 // Method 3
-  Future _showStudyNotification(DateTime scheduledTime) async {
+  Future _showStudyNotification(List<String> scheduleList) async {
     var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
       '2',
       'your channel name',
@@ -80,14 +69,17 @@ class _MyAppState extends State<MyApp> {
     );
     var platformChannelSpecifics =
         new NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.schedule(
-      1,
-      'Đi họccccccccccccccccccccc',
-      'Êi bạn êi, vô học bạn êi!!',
-      scheduledTime,
-      platformChannelSpecifics,
-      payload: '9h Thứ 3 học Computer Graphic, giờ lo làm homework đi',
-    );
+    for (var i = 0; i < 3; i++) {
+      print(i);
+      await flutterLocalNotificationsPlugin.schedule(
+        i,
+        'Đi họccccccccccccccccccccc',
+        'Êi bạn êi, vô học bạn êi!!',
+        DateTime.parse(scheduleList[i]),
+        platformChannelSpecifics,
+        payload: '9h Thứ 3 học Computer Graphic, giờ lo làm homework đi',
+      );
+    }
   }
 
   @override
@@ -102,20 +94,19 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              new SizedBox(
-                height: 30.0,
-              ),
-              new DateTimeItem(
-                  dateTime: scheduledTime,
-                  onChanged: (value) {
-                    setState(() {
-                      scheduledTime = value;
-                    });
-                  }),
+              new SizedBox(height: 30.0),
+              // new DateTimeItem(
+              //     dateTime: DateTime.parse(scheduleList[0]),
+              //     onChanged: (value) {
+              //       setState(() {
+              //         DateTime.parse(scheduleList[0]) = value;
+              //       });
+              //     }),
+              Text(scheduleList[0]),
               new ElevatedButton(
                 onPressed: () => [
-                  _showStudyNotification(scheduledTime),
-                  _showBreakNotification(scheduledTime)
+                  _showStudyNotification(scheduleList),
+                  // _showBreakNotification(scheduleList)
                 ],
                 child: new Text('Study time'),
               ),
--- local_notification/myschedule.sql
+++ local_notification/myschedule.sql
@@ -1,6 +1,12 @@
-CREATE TABLE schedule (id integer, schedule_date varchar(100));
+DROP TABLE IF EXISTS schedules;
+create table schedules (
+    id INT NOT NULL,
+    schedule_date VARCHAR(100) NOT NULL
+);
 
-insert into schedule (id, schedule_date) values (1, '2021-05-14 09:00:00');
-insert into schedule (id, schedule_date) values (2, '2021-05-14 10:00:00');
-insert into schedule (id, schedule_date) values (3, '2021-05-14 11:00:00');
-select * from schedule;
+insert into schedules (id, schedule_date) values (1, '2021-05-14 09:00:00');
+insert into schedules (id, schedule_date) values (2, '2021-05-14 10:00:00');
+insert into schedules (id, schedule_date) values (3, '2021-05-14 11:00:00');
+
+select * from schedules;
+
