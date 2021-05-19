ddcce8cb357fbd6e6983aa334aa7986afff44509
nguyenbao2301 <66950291+nguyenbao2301@users.noreply.github.com>
2021-05-19 22:21:04 +0700

    Added db models and controllers

---
+++ lib/Model/user.dart
@@ -0,0 +1,21 @@
+class User {
+  final int _id;
+  final String _username;
+  final String _fname;
+  final String _lname;
+  final String _dob;
+
+
+  User(this._id,this._username,this._fname,this._lname,this._dob) ;
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(json['id'],json['username'],json['fname'],json['lname'],json['dob']);
+  }
+  String getUsername()
+  {
+    return this._username;
+  }
+  String getName()
+  {
+    return this._fname + ' ' + this._lname;
+  }
+}
