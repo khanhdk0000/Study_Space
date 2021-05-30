d0b1935dfc3efaa5e3c08ddfe3edc4f3f3776511
dirtyg2120 <57407829+dirtyg2120@users.noreply.github.com>
2021-05-30 13:57:53 +0700

    change data retrieved format

--- lib/Notification/scheduleController.dart
+++ lib/Notification/scheduleController.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-// import 'package:study_space/constants.dart';
+import 'package:study_space/constants.dart';
 // import 'package:study_space/Model/user.dart';
-
-const webhost = "dirtyg.000webhostapp.com";
 
 class scheduleController {
   Future<String> getDate(int i) async {
     var folder = "get_date.php";
     http.Response response = await http.get(Uri.https(webhost, folder));
     var data = jsonDecode(response.body);
+    data[i] = data[i].toString();
 
-    return data[i].toString().substring(7, 17);
+    return data[i].substring(19, 23) +
+        '-' +
+        data[i].substring(13, 15) +
+        '-' +
+        data[i].substring(16, 18);
   }
 
   Future<List<String>> getStarttime() async {
@@ -21,7 +24,7 @@ class scheduleController {
     var data = jsonDecode(response.body);
 
     List<String> scheduledStudyList = [];
-    for (var i = 0; i < data.length; i++) {
+    for (var i = data.length - 3; i < data.length; i++) {
       data[i] = await getDate(i) + ' ' + data[i].toString().substring(13, 21);
       scheduledStudyList.add(data[i]);
       print(data[i]);
@@ -35,7 +38,7 @@ class scheduleController {
     var data = jsonDecode(response.body);
 
     List<String> scheduledEndtimeList = [];
-    for (var i = 0; i < data.length; i++) {
+    for (var i = data.length - 3; i < data.length; i++) {
       data[i] = await getDate(i) + ' ' + data[i].toString().substring(11, 19);
       scheduledEndtimeList.add(data[i]);
       print(data[i]);
